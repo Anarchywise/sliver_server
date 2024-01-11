2da3a0da153f7d2981c81b091c226b653d786c68
zhanghao <1981053231@qq.com>
2024-01-11 22:59:17 +0800

    inital springboot envirnment

---
+++ src/main/java/com/zh/sliver_dev_final/SliverDevFinalApplication.java
@@ -0,0 +1,13 @@
+package com.zh.sliver_dev_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SliverDevFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SliverDevFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zh/sliver_dev_final/SliverDevFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zh.sliver_dev_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SliverDevFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
